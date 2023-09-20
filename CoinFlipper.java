a0957ad2af6ba359497f967468d41ba8865aff07
João Pedro G. C <115590969+jpgercc@users.noreply.github.com>
2023-09-19 22:52:17 -0300

    try-catch

--- CoinFlipper.java
+++ CoinFlipper.java
@@ -10,13 +10,22 @@ public class CoinFlipper {
         int randomNum;
 
         System.out.println("O programa deve escolher aleatoriamente alguma opcao digitada por voce");
-        System.out.println("Selecione o numero de opçoes(até 5):");
-        numOp = scan.nextInt();
-        scan.nextLine();
 
-        if (numOp < 1 || numOp > 5) {
-            System.out.println("Número inválido de opções!");
-            return;
+        while (true) {
+            System.out.println("Selecione o numero de opçoes (até 5):");
+            String input = scan.nextLine();
+            
+            try {
+                numOp = Integer.parseInt(input);
+                
+                if (numOp >= 1 && numOp <= 5) {
+                    break; // Sai do loop se o número for válido
+                } else {
+                    System.out.println("Número inválido de opções!");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Digite apenas números!");
+            }
         }
 
         for (int i = 0; i < numOp; i++) {
